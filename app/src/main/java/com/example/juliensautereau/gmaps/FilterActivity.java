725dcbd3b06ab19f21a4b020e02a1d357fee822c
Julien Sautereau <julien.sautereau@etu.univ-lehavre.fr>
2018-02-23 16:51:55 +0100

    Modification du fichier FilterActivity.java --> Ajout 2 colonnes ('Libellé' + 'ToggleButton afficher/ne pas afficher')

--- app/src/main/java/com/example/juliensautereau/gmaps/FilterActivity.java
+++ app/src/main/java/com/example/juliensautereau/gmaps/FilterActivity.java
@@ -1,7 +1,16 @@
 package com.example.juliensautereau.gmaps;
 
+import android.database.MatrixCursor;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class FilterActivity extends AppCompatActivity {
 
@@ -9,5 +18,44 @@ public class FilterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter);
+
+        final String [] col1 = {"Marker 1","Marker 2","Marker 3","Marker 4","Marker 5"};
+        final boolean [] col2 = {true,false,true,true,true,true};
+
+        TableLayout table = (TableLayout) findViewById(R.id.idTable); // on prend le tableau défini dans le layout
+        TableRow row; // création d'un élément : ligne
+        TextView tv1;
+        ToggleButton tv2; // création des cellules
+
+        // pour chaque ligne
+        for(int i=0;i<col1.length;i++) {
+            row = new TableRow(this); // création d'une nouvelle ligne
+
+            tv1 = new TextView(this); // création cellule
+            tv1.setTextColor(Color.WHITE); // ajout de couleur
+            tv1.setText(col1[i]); // ajout du texte
+            tv1.setGravity(Gravity.CENTER); // centrage dans la cellule
+
+            // adaptation de la largeur de colonne à l'écran :
+            tv1.setLayoutParams( new TableRow.LayoutParams( 0, android.view.ViewGroup.LayoutParams.WRAP_CONTENT, 1 ) );
+
+            // idem 2ème cellule
+            tv2 = new ToggleButton(this);
+
+            // par défaut
+            tv2.setChecked(true);
+
+            // on met à jour par rapport à notre tableau
+            tv2.setChecked(col2[i]);
+            tv2.setGravity(Gravity.CENTER);
+            tv2.setLayoutParams( new TableRow.LayoutParams( 0, android.view.ViewGroup.LayoutParams.WRAP_CONTENT, 1 ) );
+
+            // ajout des cellules à la ligne
+            row.addView(tv1);
+            row.addView(tv2);
+
+            // ajout de la ligne au tableau
+            table.addView(row);
+        }
     }
 }
